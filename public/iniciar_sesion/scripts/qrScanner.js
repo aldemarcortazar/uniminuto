604cc8616a6b14d27b2eddc57fb0dd33febce135
Harol <harol.stiven147@gmail.com>
2021-07-15 22:05:11 -0500

    iniciar sesion

--- public/iniciar_sesion/scripts/qrScanner.js
+++ public/iniciar_sesion/scripts/qrScanner.js
@@ -1,3 +1,7 @@
+import {
+    ajax
+} from "./../../../js/ajax.js";
+
 var html5QrcodeScanner = new Html5QrcodeScanner(
     "reader", {
         fps: 10,
@@ -6,9 +10,9 @@ var html5QrcodeScanner = new Html5QrcodeScanner(
 
 function onScanSuccess(decodedText, decodedResult) {
     // Manejar en condición de éxito con el texto decodificado o el resultado.
-    console.log(`Scan result: ${decodedText}`, decodedResult);
+    // console.log(`Scan result: ${decodedText}`, decodedResult);
     let resultado = `${decodedText}`;
-    document.getElementById("textExample").innerHTML = resultado;
+    validarInicioSesion(resultado)
     // ...
     html5QrcodeScanner.clear();
     // ^ Detiene el escaner una vez se complete el escaneo
@@ -20,3 +24,32 @@ function onScanError(errorMessage) {
 
 
 html5QrcodeScanner.render(onScanSuccess, onScanError);
+
+function validarInicioSesion(codigo) {
+
+    fetch('include/validarInicio.php', {
+        method: 'POST', 
+        body: JSON.stringify(codigo),
+    })
+        .then(res => res.ok ? res.json() : Promise.reject(res))
+        .then( data => {
+            if(data === 0) {
+                alert("No se encontró ningún usuario")
+                window.location.reload()
+            } else {
+                localStorage.setItem("user", JSON.stringify(data))
+                let datos = parseInt(data.id_type_user)
+
+                switch(datos) {
+                    case 1: 
+                        window.location.href = "../usuarios/admin/admin.php"
+                    case 2:
+                        window.location.href = "../usuarios/admin/profesor.php"
+                }
+
+            }
+        } )
+        .catch(err => {
+            alert("Ocurrió un error")
+        })
+}
